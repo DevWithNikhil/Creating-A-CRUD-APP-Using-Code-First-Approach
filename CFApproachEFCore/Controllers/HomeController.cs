e8234b06211db7b25e44b1230a44563129b4331d
DevWithNikhil <devwithnikhil@gmail.com>
2026-04-03 19:15:10 +0530

    Added A Edit And Delete Feature

--- CFApproachEFCore/Controllers/HomeController.cs
+++ CFApproachEFCore/Controllers/HomeController.cs
@@ -51,6 +51,64 @@ namespace CFApproachEFCore.Controllers
             return View(stdData);
         }
 
+        
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || studentDb.Students == null)
+            {
+                return NotFound();
+            }
+            var stdData = await studentDb.Students.FindAsync(id);
+            if (stdData == null)
+            {
+                return NotFound();
+            }
+            return View(stdData);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int? id, Student std)
+        {
+            if (id != std.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                studentDb.Students.Update(std);
+                await studentDb.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(std);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || studentDb.Students == null)
+            {
+                return NotFound();
+            }
+            var stdData = await studentDb.Students.FirstOrDefaultAsync(x => x.Id == id);
+            if (stdData == null)
+            {
+                return NotFound();
+            }
+            return View(stdData);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var stdData = await studentDb.Students.FindAsync(id);
+            if(stdData != null)
+            {
+                studentDb.Students.Remove(stdData);
+            }
+            await studentDb.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
         public IActionResult Privacy()
         {
             return View();
